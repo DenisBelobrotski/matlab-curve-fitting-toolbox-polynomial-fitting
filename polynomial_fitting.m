5623f61a7cc0f7d236af4b6d0111fe5da6e34bb3
DenisBelobrotski <belobrotskiy@gmail.com>
2021-01-09 14:57:49 +0300

    Initial commit

---
+++ polynomial_fitting.m
@@ -0,0 +1,22 @@
+% Sixth degree polynomial fitting
+
+load data_file;
+x = x';
+y = y';
+
+figure
+plot(x, y, '.');
+title('Input data');
+
+% built-in sixth degree polynomial
+%[population, gof] = fit(x, y, 'poly6');
+
+% custom model (polynomial)
+model = 'p_6 * x^6 + p_5 * x^5 + p_4 * x^4 + p_3 * x^3 + p_2 * x^2 + p_1 * x + p_0';
+[population, gof] = fit(x, y, model);
+
+figure
+plot(population, x, y);
+title('Polynomial fitting');
+
+disp(population);
